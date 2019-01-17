940304ddca4c471b1e39af54947b6e04dd7b3533
Manlove_Logan <manlove.logan@gmail.com>
2019-01-16 22:37:07 -0600

    started building main application and head node.

--- LendingClubAnalysis.py
+++ LendingClubAnalysis.py
@@ -1,5 +1,25 @@
 # Lending Club Data Analysis
 
-class data_node():
+class main():
+    def __init__(self):
+        files = ['a', 'b', 'c']
+        head = head_node()
+        for file in files:
+            head.parse_file(file)
+
+
+class head_node():
     def __init__(self):
         pass
+
+    def parse_file(self, file_path):
+        with open(file_path, 'r') as file:
+            for line in file.readlines():
+                pass
+
+class data_node():
+    def __init__(self):
+        node_title = ""
+        node_response = ""
+        node_count = 0
+        node_sum = 0
