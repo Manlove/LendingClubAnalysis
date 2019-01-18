dfdaeb368936675eb78ef6e3da901583edce4451
Manlove_Logan <manlove.logan@gmail.com>
2019-01-17 23:08:50 -0600

    merged main and old head_node classes, created new head class

--- LendingClubAnalysis.py
+++ LendingClubAnalysis.py
@@ -5,13 +5,10 @@ class main():
         #file = "C:\\Users\\M113455\\Desktop\\LoanStats_2018Q3.csv"
         files = ["C:\\Users\\manlo\\Desktop\\LendingClubData\\LoanStats_2018Q1.csv"]
         fields = fields
-        head = head_node()
-        head.retrieve_header(files[0])
-        head.retrieve_fields(fields)
+        self.retrieve_header(files[0])
+        self.retrieve_fields(fields)
         for file in files:
-            head.parse_file(file)
-
-class head_node():
+            self.parse_file(file)
 
     def parse_file(self, file_path):
         with open(file_path, 'r', errors='ignore') as file_in:
@@ -21,9 +18,10 @@ class head_node():
                 if line[16] == 'Current':
                     continue
                 records = [line[i] for i in self.field_inds]
-                print(records)
-                if j > 1:
-                    break
+                self.fill_tree(records)
+
+    def fill_tree(self, records):
+
 
     def retrieve_fields(self, fields):
         # Takes the user inputted fields of interest and finds the indicies of the item in the header file
@@ -46,12 +44,18 @@ class head_node():
                 line[-1] = line[-1][:-1]
             self.header = line
 
+class head_node():
+    def __init__(self):
+        self.node_count = 0
+        self.node_sum = 0
+
 class data_node():
     def __init__(self):
-        node_title = ""
-        node_response = ""
-        node_count = 0
-        node_sum = 0
+        self.node_title = ""
+        self.node_response = ""
+        self.node_count = 0
+        self.node_sum = 0
+        self.branches = 0
 
 if __name__ == "__main__":
     app = main(["id", "member_id", "loan_amnt", "settlement_amount"])
