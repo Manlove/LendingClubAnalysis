9f74cca72c8c9da2216eda1d0c1a2406dce7ee2d
Manlove <Manlove.Logan@mayo.edu>
2019-01-21 10:55:39 -0600

    added some commenting, magic add method for adding node values and counts

--- LendingClubAnalysis.py
+++ LendingClubAnalysis.py
@@ -2,19 +2,26 @@
 import re
 class main():
     def __init__(self, fields):
-        #file = "C:\\Users\\M113455\\Desktop\\LoanStats_2018Q3.csv"
-        files = ["C:\\Users\\manlo\\Desktop\\LendingClubData\\LoanStats_2018Q1.csv"]
+        files = ["C:\\Users\\M113455\\Desktop\\LoanStats_2018Q3.csv"]
+        #files = ["C:\\Users\\manlo\\Desktop\\LendingClubData\\LoanStats_2018Q1.csv"]
         fields = fields
-        self.head_node = data_node("head", level = 0)
+        self.head_node = data_node("head")
         self.retrieve_header(files[0])
         self.retrieve_fields(fields)
         for file in files:
             self.parse_file(file)
 
     def parse_file(self, file_path):
+        """
+        Takes a file path for a .csv file formatted with '","' as a seperator
+        and retrieves the fields that were selected by the user along with the binary
+        output of the loan_status column
+
+        Calls the fill_tree function on the records and the values
+        """
         with open(file_path, 'r', errors='ignore') as file_in:
             next(file_in)
-            for j, line in enumerate(file_in.readlines()):
+            for line in file_in.readlines():
                 line = line.strip().split('","')
                 line[0] = ""
                 if line[16] == 'Current':
@@ -31,31 +38,31 @@ class main():
 
         for record, field in zip(records, self.fields):
             if not len(current_node.branches):
-                branch = data_node(field, level = current_node.lvl + 1)
+                branch = data_node(field)
                 current_node.branches.append(branch)
                 current_node = branch
                 current_node.node_response = record
-                current_node.node_count += 1
-                current_node.node_sum += value
+                current_node += value
             else:
                 match = 0
                 for branch in current_node.branches:
                     if branch.node_title == field and branch.node_response == record:
                         current_node = branch
-                        current_node.node_count += 1
-                        current_node.node_sum += value
+                        current_node += value
                         match = 1
                 if match == 0:
-                    branch = data_node(field, level = current_node.lvl + 1)
+                    branch = data_node(field)
                     branch.node_response = record
-                    branch.node_count += 1
-                    branch.node_sum += value
+                    branch += value
                     current_node.branches.append(branch)
                     current_node = branch
             #print("\t" * current_node.lvl + str(current_node.node_title) + " " + record + " " + str(current_node.node_response))
 
     def retrieve_fields(self, fields):
-        # Takes the user inputted fields of interest and finds the indicies of the item in the header file
+        """
+        Takes the user inputted fields of interest and finds the indicies of the item in the header file.
+        Stores the fields and the field indicies.
+        """
         self.fields = []
         self.field_inds = []
         for field in fields:
@@ -66,7 +73,9 @@ class main():
                 self.field_inds.append(self.header.index(field))
 
     def retrieve_header(self, file_path):
-        # Retrieve the header from the given .csv file and returns a list of the fields
+        """
+        Retrieve the header from the given .csv file and stores a list of the fields
+        """
         with open(file_path, 'r', errors='ignore') as  file_in:
             line = file_in.readline()
             line = [item.lower() for item in line.strip().split('","')]
@@ -82,6 +91,11 @@ class data_node():
         self.node_count = 0
         self.node_sum = 0
         self.branches = []
+
+    def __add__(self, value):
+        self.node_count += 1
+        self.node_sum += value
+        return self
 
 if __name__ == "__main__":
     app = main(["term", "loan_amnt", "settlement_amount"])
